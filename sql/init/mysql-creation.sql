b12d2cd8b90c470c75fd41458fdebac392e401c6
JKal24 <jkalirai2499@gmail.com>
2021-05-21 01:23:04 -0400

    More DB changes

--- sql/init/mysql-creation.sql
+++ sql/init/mysql-creation.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `smite_api`;
+CREATE DATABASE IF NOT EXISTS smite_api;
 
 CREATE USER 'jask24'@'localhost';
 GRANT ALL ON smite_api TO 'jask24'@'localhost' IDENTIFIED BY 'Brave99_';
