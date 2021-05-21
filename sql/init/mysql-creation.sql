7b525eefb867a0e8a9d6adf7c3a37d265c190d34
JKal24 <jkalirai2499@gmail.com>
2021-05-21 01:12:48 -0400

    Changing DB structure

--- sql/init/mysql-creation.sql
+++ sql/init/mysql-creation.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `smite_api`;
-CREATE DATABASE IF NOT EXISTS `smite_session`;
+
+CREATE USER 'jask24'@'localhost';
+GRANT ALL ON smite_api TO 'jask24'@'localhost' IDENTIFIED BY 'Brave99_';
